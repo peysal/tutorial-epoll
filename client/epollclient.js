9586d0e26766113dbb81f97e9e1d89b0e7268b8c
peysal <peysal@gmail.com>
2014-01-08 23:49:23 +0800

    remove insecure package and ability to add questions

--- client/epollclient.js
+++ client/epollclient.js
@@ -1,11 +1,11 @@
-Template.hello.greeting = function () {
-    return "The Missing Meteor Tutorial!!!";
-};
+Template.addquestion.events({
+    'click input.add-question' : function(event){
+        event.preventDefault();
+        var questionText = document.getElementById("questionText").value;
+        Meteor.call("addQuestion",questionText,function(error , questionId){
+          console.log('added question with Id .. '+questionId);
+        });
+        document.getElementById("questionText").value = "";
  
-Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
     }
 });
--- server/epollclient.js
+++ server/epollclient.js
@@ -1,3 +1,17 @@
+Questions = new Meteor.Collection("questions");
+ 
 Meteor.startup(function () {
     // code to run on server at startup
- });
+});
+ 
+Meteor.methods({
+  addQuestion : function(questionText){
+    console.log('Adding Question');
+    var questionId = Questions.insert({
+          'questionText' : questionText,
+          'submittedOn': new Date(),
+          'submittedBy' : Meteor.userId()
+      });
+    return questionId;
+  }
+});
